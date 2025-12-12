013023bae1b87ea03ff209d1b937fb3d4eac1360
ESS48 <esantistebans01@educantabria.com>
2025-12-12 21:12:32 +0100

    Ahorcado segundo commit

--- tema 3/ejercicios/Ahorcado/src/main/java/com/agl/ahorcado/Ahorcado.java
+++ tema 3/ejercicios/Ahorcado/src/main/java/com/agl/ahorcado/Ahorcado.java
@@ -4,6 +4,7 @@
 package com.agl.ahorcado;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -15,42 +16,32 @@ public class Ahorcado {
 
         String[] palabras = {"fresa", "vainilla", "chocalate"};
         String palabra = palabras[(int) (Math.random() * 3)];
-        
+
         for (int i = 0; i < palabras.length; i++) {
-            palabras[i]=palabra;
+            palabras[i] = palabra;
             System.out.println(palabras[i]);
         }
         
-        
-        
-        
-        String guion=guiones(palabras);
-        System.out.println(guion);
-        
-        
-        
-        //switch (palabra) {
-        //case 1:
-        //System.out.println("fresa");
-        //break;
-        //case 2:
-        //System.out.println("vainilla");
-        //break;
-        //case 3:
-        //System.out.println("chocalate");
-        //break;
-        //default:
-        //} 
-    }
-
-    public static String guiones(String[] guiones) {
-
-        String guion=("_");
-        for (int i = 1; i < guiones.length; i++) {
-            guiones[i] = guion;
+        String guion = "_";
+        for (int i = 0; i < palabras.length; i++) {
+            palabras[i] = palabra;
+            System.out.print(palabra=guion+" ");
         }
-
-        return guion;
-
+        
+        
+        
+        //char[]secreto;
     }
+
+    //public static String guiones(String[] guiones) {
+        //String guion = "_";
+        //int G = Integer.parseInt(guion);
+
+        //for (int i = 1; i < guiones.length; i++) {
+            //System.out.println(guiones.charAt(i));
+        //}
+
+        //return guion;
+
+    //}
 }
