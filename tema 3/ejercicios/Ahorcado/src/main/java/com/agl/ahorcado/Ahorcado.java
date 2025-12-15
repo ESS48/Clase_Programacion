10eaffdac9d05abf9b9851d4b3b745c6c8655471
ESS48 <esantistebans01@educantabria.com>
2025-12-15 19:59:46 +0100

    Ahorcado tercer commit

--- tema 3/ejercicios/Ahorcado/src/main/java/com/agl/ahorcado/Ahorcado.java
+++ tema 3/ejercicios/Ahorcado/src/main/java/com/agl/ahorcado/Ahorcado.java
@@ -3,7 +3,6 @@
  */
 package com.agl.ahorcado;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -13,35 +12,52 @@ import java.util.Scanner;
 public class Ahorcado {
 
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
 
         String[] palabras = {"fresa", "vainilla", "chocalate"};
         String palabra = palabras[(int) (Math.random() * 3)];
 
-        for (int i = 0; i < palabras.length; i++) {
-            palabras[i] = palabra;
-            System.out.println(palabras[i]);
+        System.out.println("La palabra para ver cual es: " + palabra);
+
+        for (int i = 0; i < palabra.length(); i++) {
+            System.out.print("_ ");
         }
-        
-        String guion = "_";
-        for (int i = 0; i < palabras.length; i++) {
-            palabras[i] = palabra;
-            System.out.print(palabra=guion+" ");
+
+        System.out.println("Inserta una letra: ");
+        String letra = teclado.nextLine();
+
+        String buscarLetra, texto;
+        int posicion;
+
+        texto = palabra;
+        buscarLetra = letra;
+
+        posicion = texto.indexOf(buscarLetra);
+
+        if (posicion > 0) {
+            System.out.println("Letra correcta");
+            posicion = texto.indexOf(letra, posicion + 1);
+        } else {
+            System.out.println("Letra incorrecta");
         }
-        
-        
-        
-        //char[]secreto;
+
+        char[]secreto;
     }
-
+    
+    // array para los guiones. 
+    
+    //Pruebas
+    //for (int i = 0; i < palabras.length; i++) {
+    //palabras[i] = palabra;
+    //System.out.println(palabras[i]);
+    //}
+    
     //public static String guiones(String[] guiones) {
-        //String guion = "_";
-        //int G = Integer.parseInt(guion);
-
-        //for (int i = 1; i < guiones.length; i++) {
-            //System.out.println(guiones.charAt(i));
-        //}
-
-        //return guion;
-
+    //String guion = "_";
+    //int G = Integer.parseInt(guion);
+    //for (int i = 1; i < guiones.length; i++) {
+    //System.out.println(guiones.charAt(i));
+    //}
+    //return guion;
     //}
 }
