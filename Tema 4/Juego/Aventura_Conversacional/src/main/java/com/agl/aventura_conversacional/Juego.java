ddc40c3f4cebdd847253cb3edd8b7758639f7ace
ESS48 <esantistebans01@educantabria.com>
2026-01-30 19:35:45 +0100

    mas proceso de la clase juego..

--- Tema 4/Juego/Aventura_Conversacional/src/main/java/com/agl/aventura_conversacional/Juego.java
+++ Tema 4/Juego/Aventura_Conversacional/src/main/java/com/agl/aventura_conversacional/Juego.java
@@ -14,9 +14,12 @@ public class Juego {
     private String introduccicon;
     private int menu;
     private String escena1;
+    
+    
+    public Juego(){
+    
+    }
 
-    
-    
     public int getSeleccion() {
         return seleccion;
     }
@@ -49,41 +52,41 @@ public class Juego {
         this.escena1 = escena1;
     }
 
-    
-    
-    
-    
-    
-    
     public void introduccicon() {
+
+        System.out.println("Entras a una cueva, donde te pierdes y encuentras 4 caminos donde si o si tienes que elegir uno. " + this.introduccicon);
+
+    }
+
+    public void menu(int opcion) {
         
-        System.out.println("Entras a una cueva, donde te pierdes y encuentras 4 caminos donde si o si tienes que elegir uno. "+this.introduccicon);
-        
+        System.out.println("Eleccion: "+this.menu);
+        switch (opcion) {
+            case 1:
+                System.out.println();
+                break;
+                
+            default:
+                System.out.println("No hay ninguna opcion de la elegida.");
+        }
+
     }
-    
-    
-    public void menu(){
-    
-    
-    
-    
+
+    public void escena1() {
+
+        System.out.println("Al ir por este camino, te topas con un enemigo y empieza un combate inesperaso. " + this.escena1);
+
     }
-    
-    
-    public void escena1(){
-    
-    
+
+    public void escena2() {
+
     }
-    public void escena2(){
-    
-    
+
+    public void escena3() {
+
     }
-    public void escena3(){
-    
-    
-    }
-    public void escena4(){
-    
-    
+
+    public void escena4() {
+
     }
 }
