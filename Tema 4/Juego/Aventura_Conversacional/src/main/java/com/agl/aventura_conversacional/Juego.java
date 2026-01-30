3615e6df253f5fa17a71650d7c39ce4e92f4a993
ESS48 <esantistebans01@educantabria.com>
2026-01-30 20:04:59 +0100

    metodo combate

--- Tema 4/Juego/Aventura_Conversacional/src/main/java/com/agl/aventura_conversacional/Juego.java
+++ Tema 4/Juego/Aventura_Conversacional/src/main/java/com/agl/aventura_conversacional/Juego.java
@@ -4,6 +4,9 @@
  */
 package com.agl.aventura_conversacional;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author damt119
@@ -71,7 +74,7 @@ public class Juego {
 
     public void introduccicon() {
 
-        System.out.println("Entras a una cueva, donde te pierdes y encuentras 4 caminos donde si o si tienes que elegir uno. " + this.introduccicon);
+        System.out.println("Estas desorientado y sin querer entras a una cueva, donde te pierdes y encuentras 4 caminos donde si o si tienes que elegir uno. " + this.introduccicon);
 
     }
 
@@ -93,5 +96,81 @@ public class Juego {
     public void escena1() {
         System.out.println("Al ir por este camino, te topas con un enemigo y empieza un combate inesperaso. ");
 
+    }
+    
+    
+    public void combate(){
+    
+    Scanner teclado = new Scanner(System.in);
+        String nombre;
+        int opcion;
+
+        System.out.println("Dime tu nombre de personaje: ");
+        nombre = teclado.next();
+
+        Arma a1 = new Arma("Cuchillo ", 5);
+        Jugador jugador = new Jugador(nombre, 12, a1);
+        Enemigo mounstro = new Enemigo("Goblin ", 15, 4);
+
+        System.out.println("¡Que empieze el combate!");
+
+        do {
+
+            System.out.println("Jugador: " + jugador.getNombre());
+            System.out.println("Vida: " + jugador.getvida());
+            System.out.println("Arma: " + jugador.getArma().getDanioMaximo());
+
+            System.out.println("Mounstro: " + mounstro.getTipo());
+            System.out.println("Vida: " + mounstro.getvida());
+            System.out.println("Daño máx: " + mounstro.getDanioMaximo());
+
+            System.out.println("Escoge una opción: ");
+            System.out.println("1. Atacar");
+            System.out.println("2. Curarte");
+            System.out.println("3. Huir");
+            opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    Random dado = new Random();
+                    int ataque = dado.nextInt(jugador.getArma().getDanioMaximo());
+                    mounstro.restarHP(ataque);
+                    System.out.println("Daño: " + ataque);
+                    
+                    break;
+                case 2:
+
+                    jugador.sumarHP(2);
+                    System.out.println("Te curaste 2 puntos de vida.");
+                    break;
+
+                case 3:
+
+                    System.out.println("Has huido");
+                    System.out.println("FIN DEL JUEGO");
+
+                    break;
+
+                default:
+                    System.out.println("Intentelo de nuevo.");
+
+            }
+
+            //Turno mounstro
+            if (opcion == 1 || opcion == 2) {
+                Random r1 = new Random();
+                int golpe = r1.nextInt(mounstro.getDanioMaximo());
+                jugador.restarHP(golpe);
+                System.out.println("El goblin te ataca y te hace " + golpe + " de daño.");
+            }
+
+        } while (jugador.getvida() > 0 && mounstro.getvida() > 0 && opcion != 3);
+
+        if (jugador.getvida() == 0) {
+            System.out.println("GAME OVER");
+        } else if (mounstro.getvida() == 0) {
+            System.out.println("¡Has Ganado!");
+        }
+    
     }
 }
