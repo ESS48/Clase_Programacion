db49aa22d72bfc028a59915de8012e9550ffae7c
ESS48 <esantistebans01@educantabria.com>
2026-03-27 20:56:19 +0100

    fase 2 personaje

---
+++ Tema 4/Juego/Aventura_Conversacional/src/main/java/com/agl/aventura_conversacional/Personaje.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.agl.aventura_conversacional;
+
+/**
+ *
+ * @author damt119
+ */
+public class Personaje {
+    private int vida;
+
+    public Personaje(int vida) {
+        this.vida = vida;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
+    
+}
